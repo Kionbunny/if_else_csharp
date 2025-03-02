28d3acf18b725f8c33abd468ecac6a13e2f61bfb
masha <bunnymummy2002@gmail.com>
2025-03-02 15:26:41 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Top level statement allow developers to write a program without the need for a main method or a class 
+Console.WriteLine("Hello");
+string message = "hello world!";
+Console.WriteLine(message);
+if (message.Contains("h"))
+{
+    Console.WriteLine("status ok ");
+}
+else
+{
+    Console.WriteLine("status not ok ");
+}
